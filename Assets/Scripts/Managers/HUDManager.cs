dd9ef765f2a8a35cf1bfe923862c5acf4741d4fb
galliotgreg <galliotgregpro@gmail.com>
2017-11-25 21:58:43 +0100

    PopupV2
    Text adaptatif

--- Assets/Scripts/Managers/HUDManager.cs
+++ Assets/Scripts/Managers/HUDManager.cs
@@ -13,6 +13,8 @@ public class HUDManager : MonoBehaviour {
     public Bulle bulleCree;
     public ParticleSystem explosion;
 
+    enum Declencheurs {FLECHETTES, SHOOT, K_COOP, K_SOLO, MIN, CLIENT1, TREIZE, STUN, STUCK, CLIENT2, POURCENT1, PARTIE1, PARTIE2, GEEK, WOMEN, MEN, SECONDE, RECOVER, DISTRI, DOORS, CALL};      
+
     // Use this for initialization
     void Start () {       
         playerCurrentLife = playerTotalLife;
@@ -63,11 +65,91 @@ public class HUDManager : MonoBehaviour {
         transClone.localScale = new Vector3(1, 1, 1);
         Bulle clone = (Bulle)Instantiate(bulleCree, this.transform, true);
         clone.timeoutDestructor = 1.5f;
-        clone.SetText(text);
+
+        int rand = Random.Range(0,(int)Declencheurs.CALL);
+
+        clone.SetText(TextResolver((Declencheurs)rand));
     }
 
     public void BulleExplosion()
     {
         ParticleSystem clone = (ParticleSystem)Instantiate(explosion, this.transform, true);
+        Destroy(clone.gameObject, 5);
+    }
+
+    string TextResolver(Declencheurs textEnum)
+    {
+        string textRes = "";
+        switch (textEnum)
+        {
+            case Declencheurs.CALL:
+                textRes = "13 APPELS DU CHEF";                
+                break;
+            case Declencheurs.CLIENT1:
+                textRes = "13 CLIENTS";                
+                break;
+            case Declencheurs.CLIENT2:
+                textRes = "13 CLIENTS 2";                
+                break;
+            case Declencheurs.DISTRI:
+                textRes = "13 LIVRAISONS";                
+                break;
+            case Declencheurs.DOORS:
+                textRes = "13 PORTES";                
+                break;
+            case Declencheurs.FLECHETTES:
+                textRes = "13 FLECHETTES";                
+                break;
+            case Declencheurs.GEEK:
+                textRes = "13 GEEK";                
+                break;
+            case Declencheurs.K_COOP:
+                textRes = "13 STOP COOP";                
+                break;
+            case Declencheurs.K_SOLO:
+                textRes = "13 STOP SOLO";                
+                break;
+            case Declencheurs.MEN:
+                textRes = "13 HOMMES";                
+                break;
+            case Declencheurs.MIN:
+                textRes = "13 MINUTES";                
+                break;
+            case Declencheurs.PARTIE1:
+                textRes = "13 PARTIE";                
+                break;
+            case Declencheurs.PARTIE2:
+                textRes = "13 PARTIE 2";                
+                break;
+            case Declencheurs.POURCENT1:
+                textRes = "13 POURCENTS";                
+                break;
+            case Declencheurs.RECOVER:
+                textRes = "13 RECOVER";                
+                break;
+            case Declencheurs.SECONDE:
+                textRes = "13 SECONDES";                
+                break;
+            case Declencheurs.SHOOT:
+                textRes = "13 SHOOTS";                
+                break;
+            case Declencheurs.STUCK:
+                textRes = "13 GUN FAILS";                
+                break;
+            case Declencheurs.STUN:
+                textRes = "13 STUN";                
+                break;
+            case Declencheurs.TREIZE:
+                textRes = "13 x 13";               
+                break;
+            case Declencheurs.WOMEN:
+                textRes = "13 FEMMES";                
+                break;           
+            default:
+                Debug.Log("Default case");
+                break;
+        }
+
+        return textRes;
     }
 }
