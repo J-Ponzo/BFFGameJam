357382d32cec318c1c0b181033b6d0a4d6b8395a
Narlugo <narutogoku2@hotmail.fr>
2017-11-26 05:53:31 +0100

    adding some prefab and script

---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour {
+
+    public Transform bullet;
+    // Use this for initialization
+    void Start() {
+        Debug.Log(bullet.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+
+    public void Shoot() {
+        Instantiate(bullet, transform.position,transform.rotation);
+    }
+
+
+    
+}
+
