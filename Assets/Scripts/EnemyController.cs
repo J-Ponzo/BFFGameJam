1a0e129201b0a5d826f3c9c6c227cf6c9026692a
J-Ponzo <jeremy.ponzo@gmail.com>
2017-11-26 04:38:55 +0100

    Create Enemy and nav mesh

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour {
+    [SerializeField]
+    private Vector3 trgPos;
+    [SerializeField]
+    private NavMeshAgent agent;
+
+    public Vector3 TrgPos
+    {
+        get
+        {
+            return trgPos;
+        }
+
+        set
+        {
+            trgPos = value;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        agent = GetComponent<NavMeshAgent>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        agent.SetDestination(trgPos);
+    }
+}
