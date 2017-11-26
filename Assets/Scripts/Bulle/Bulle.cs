0d749a8765f4798055573093672e97ccae5cb4a4
galliotgreg <galliotgregpro@gmail.com>
2017-11-26 11:42:03 +0100

    PopUpV8

--- Assets/Scripts/Bulle/Bulle.cs
+++ Assets/Scripts/Bulle/Bulle.cs
@@ -11,22 +11,28 @@ public class Bulle : MonoBehaviour {
     public RawImage imageBulle;
     public ParticleSystem explosion;
     public Transform player;
+    private bool affichage = true;
 
     // Use this for initialization
     void Start () {
-        hUDManager = transform.parent.gameObject.GetComponent<HUDManager>();
+        hUDManager = transform.parent.gameObject.GetComponent<HUDManager>();        
+        this.transform.Translate(new Vector3(0, 0, 0.36f));
         Destroy(this.gameObject, timeoutDestructor);
 	}
-	
-	// Update is called once per frame
-	void Update () {
 
-        Debug.Log(Mathf.Abs(this.transform.position.z - player.position.z));
-        Debug.Log(this.transform.position.z);
-        Debug.Log(player.position.z);
-        if (Mathf.Abs(this.transform.position.z - player.position.z) > 0.35) {            
-            this.transform.Translate(new Vector3(0, 0, -2.5f));
-        }
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Debug.Log("Distance entre la bulle et la position du joueur" + Mathf.Abs(this.transform.position.z - player.position.z));
+        //if (this.transform.position.z - player.position.z < 0.35)
+        //{
+        //    this.transform.Translate(new Vector3(0, 0, 0.1f));
+        //}
+        //else if (this.transform.position.z - player.position.z > 0.35)
+        //{
+        //    this.transform.Translate(new Vector3(0, 0, -0.1f));
+        //}
     }
 
     private void OnDestroy()
